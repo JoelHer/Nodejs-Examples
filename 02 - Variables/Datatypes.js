df518716f9a31bc725adb3fda490294d0d2050fa
JoelHer <JoelHerbst07@gmail.com>
2022-10-11 14:34:05 +0200

    02 - Variables

---
+++ 02 - Variables/Datatypes.js
@@ -0,0 +1,7 @@
+stringVariable     = "I am a string"
+integerVariable    = 123
+decimalVariable    = 123.12334
+booleanVariable    = true
+charVariable       = 'A'
+listIntVariable    = [1,2,3,4,5,6,7,8,9]
+listStringVariable = ["H", "E", "L", "L", "O"]
---
+++ 02 - Variables/Math.js
@@ -0,0 +1,4 @@
+a = 12
+b = 3
+
+console.log(a+b)
